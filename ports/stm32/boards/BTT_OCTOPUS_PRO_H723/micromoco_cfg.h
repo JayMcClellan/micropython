72edfb2904bbfbbc45157ca822619d9dc45610bb
Jay McClellan <Jay@BrainRight.com>
2026-08-29 12:30:54 -0400

    WIP, solver refactor

--- ports/stm32/boards/BTT_OCTOPUS_PRO_H723/micromoco_cfg.h
+++ ports/stm32/boards/BTT_OCTOPUS_PRO_H723/micromoco_cfg.h
@@ -14,7 +14,7 @@
 typedef float moco_float;
 
 #define MOCO_PLAN_FALLBACK_US 100
-#define MOCO_PLAN_CHUNK_CHANNELS 2
+#define MOCO_PLAN_CHUNK_CHANNELS 8
 
 // The port's own libm is built for MICROPY_FLOAT_IMPL (double on this
 // board), so it doesn't provide a real sqrtf() symbol -- and __builtin_sqrtf
